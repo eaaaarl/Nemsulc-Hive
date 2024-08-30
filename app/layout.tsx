34602f0af2e245559fff4b806766a473e869d143
eaaaarl <dev.earl00@gmail.com>
2024-08-30 15:11:11 +0800

    Nemsu Hive

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Simple Web app",
+  title: "NEMSU-LC HIVE",
 };
 
 export default function RootLayout({
